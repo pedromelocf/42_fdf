20c7a8401cd2e9658d3f41343c4a9099c87b4aa3
Pedro Melo <pedromelocf@yahoo.com>
2023-11-20 17:23:15 -0300

    Inserting first pixels "correctly" in window

--- fdf_utils/draw_pixels.c
+++ fdf_utils/draw_pixels.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 20:33:21 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/11/18 21:08:07 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/11/20 17:19:24 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ void	draw_pixels(char **map, mlx_image_t *img)
         while (map[y][x] != '\0')
         {
             if (map[y][x] == '0')
-                mlx_put_pixel(img, x, y, 0xFFFFFF);
-            else if (map[y][x] == '1')
-                mlx_put_pixel(img, x, y, 0xFFFFFF);
+                mlx_put_pixel(img, x * 20, y * 20, 0xFFFFFF);
+            if (map[y][x] == '1')
+                mlx_put_pixel(img, x * 20, y * 20, 0xFFFFFF);
             x++;
         }
         y++;
--- fdf_utils/map_read.c
+++ fdf_utils/map_read.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 18:06:23 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/11/18 21:03:19 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/11/20 17:07:39 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,7 @@ char		**map_read(const char *argv)
 	fd = open(argv, O_RDONLY);
 	bytesread = read(fd, map, 1000);
 	map[bytesread] = '\0';
-	test = ft_split(map, ' ');
-	for (int i = 0; test[i] != NULL; i++) {
-        ft_printf("%s\n", test[i]);
-	}
+	test = ft_split(map, '\n');
 	close(fd);
 	return (test);
 }
