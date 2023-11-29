71c1fabfb97000fcfa124679defa8ee934953ff5
Pedro Melo <pedromelocf@yahoo.com>
2023-11-29 14:21:46 -0300

    removing matrix values

--- fdf_utils/draw_pixels.c
+++ fdf_utils/draw_pixels.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 20:33:21 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/11/29 14:11:34 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/11/29 14:18:55 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,18 +58,12 @@ float    **get_map_matrix(t_map *s_map)
 {
     float    **map_matrix;
     int     x;
-    int     y;
 
     x = 0;
-    y = 0;
     map_matrix = malloc(sizeof(float *) * s_map->height * s_map->width + 1);
     while (s_map != NULL)
     {
-        y = 0;
         map_matrix[x] = malloc(sizeof(float) * 2);
-        map_matrix[x][y] = s_map->s_coordinate->x;
-        y++;
-        map_matrix[x][y] = s_map->s_coordinate->y;
         x++;
         s_map = s_map->next;
     }
