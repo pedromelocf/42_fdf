df91f8c8085efaa645994e292c42ed07ee844f02
Pedro Melo <pedromelocf@yahoo.com>
2023-11-30 12:06:19 -0300

    Refactoring alloc matrix to get matrix. This will increase the performance of my program

--- fdf_utils/draw_pixels.c
+++ fdf_utils/draw_pixels.c
@@ -6,30 +6,31 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 20:33:21 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/11/29 14:22:50 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/11/30 12:05:22 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/fdf.h"
 
-#include <math.h>
-
 void draw_pixels(t_map *s_map, mlx_image_t *img)
 {
     float    **map_matrix;
-    int x = 0;
-    int y = 0;
+    int     x;
+    int     y;
 
+    x = 0;
+    y = 0;
     draw_background(img);
-    map_matrix = alloc_map_matrix(s_map);
-    while (s_map != NULL)
+    map_matrix = get_map_matrix(s_map);
+    while (x < s_map->width * s_map->height)
     {
         y = 0;
-        map_matrix[x][y] = (s_map->s_coordinate->x - s_map->s_coordinate->y) * 0.707106781;
-        y++;
-        map_matrix[x][y] = ((s_map->s_coordinate->x + s_map->s_coordinate->y) * 0.40824829) - 0.816496581 * s_map->s_coordinate->z;
+        while (y < 1)
+        {
+            printf("Pos %d\n%f\n%f\n\n", x , map_matrix[x][y], map_matrix[x][y +1]);
+            y++;
+        }
         x++;
-        s_map = s_map->next;
     }
     return ;
 }
@@ -54,16 +55,22 @@ void    draw_background(mlx_image_t *img)
     return;
 }
 
-float    **alloc_map_matrix(t_map *s_map)
+float    **get_map_matrix(t_map *s_map)
 {
     float    **map_matrix;
     int     x;
+    int     y;
 
+    y = 0;
     x = 0;
     map_matrix = malloc(sizeof(float *) * s_map->height * s_map->width + 1);
     while (s_map != NULL)
     {
+        y = 0;
         map_matrix[x] = malloc(sizeof(float) * 2);
+        map_matrix[x][y] = (s_map->s_coordinate->x - s_map->s_coordinate->y) * 0.707106781;
+        y++;
+        map_matrix[x][y] = ((s_map->s_coordinate->x + s_map->s_coordinate->y) * 0.40824829) - 0.816496581 * s_map->s_coordinate->z;
         x++;
         s_map = s_map->next;
     }
